fde0508d0f1c6efb4b5a60d470cabc4940b9a64c
golgt <ovco.sk@gmail.com>
2025-03-09 11:14:24 +0100

    pridenie.json

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php include 'functions.php'; ?>
+<?php $banner = getBannerData(); ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -47,32 +49,32 @@ https://templatemo.com/tm-589-lugx-gaming
   <?php include 'header.html'; ?>
   <!-- ***** Header Area End ***** -->
 
-  <div class="main-banner">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-6 align-self-center">
-          <div class="caption header-text">
-            <h6>Welcome to lugx</h6>
-            <h2>BEST GAMING SITE EVER!</h2>
-            <p>LUGX Gaming is free Bootstrap 5 HTML CSS website template for your gaming websites. You can download and use this layout for commercial purposes. Please tell your friends about TemplateMo.</p>
-            <div class="search-input">
-              <form id="search" action="#">
-                <input type="text" placeholder="Type Something" id='searchText' name="searchKeyword" onkeypress="handle" />
-                <button role="button">Search Now</button>
-              </form>
-            </div>
+  <<div class="main-banner">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6 align-self-center">
+        <div class="caption header-text">
+          <h6><?php echo $banner["subtitle"]; ?></h6>
+          <h2><?php echo $banner["title"]; ?></h2>
+          <p><?php echo $banner["description"]; ?></p>
+          <div class="search-input">
+            <form id="search" action="#">
+              <input type="text" placeholder="Type Something" id='searchText' name="searchKeyword" />
+              <button role="button">Search Now</button>
+            </form>
           </div>
         </div>
-        <div class="col-lg-4 offset-lg-2">
-          <div class="right-image">
-            <img src="assets/images/banner-image.jpg" alt="">
-            <span class="price">$22</span>
-            <span class="offer">-40%</span>
-          </div>
+      </div>
+      <div class="col-lg-4 offset-lg-2">
+        <div class="right-image">
+          <img src="<?php echo $banner["image"]; ?>" alt="">
+          <span class="price"><?php echo $banner["price"]; ?></span>
+          <span class="offer"><?php echo $banner["offer"]; ?></span>
         </div>
       </div>
     </div>
   </div>
+</div>
 
   <div class="features">
     <div class="container">
